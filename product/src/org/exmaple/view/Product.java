1569ebd427b2d7b4db5d96e68f7f9056857e2e76
ACER <ACER@DESKTOP-KSV5FJM>
2022-08-15 12:38:51 +0530

    A2

---
+++ product/src/org/exmaple/view/Product.java
@@ -0,0 +1,27 @@
+package org.exmaple.view;
+
+public class MainView {
+	//Hint
+	int number=10;
+	String name="Kalai";
+	String name = new String("Kalai");
+	System.out.println(number); // output: 10;
+	System.out.println("The number is "+number); // output: The number is 10
+	
+	// Define a method that accepts Customer Object and returns customer Id
+	class Customer getCustomer{
+		
+	}
+	
+	// Define a method that accepts Product object and return product name
+	
+	//Define a method that accepts customer id and prints customer name inside the method definition
+// Define a method that accepts product price in the method definition, 
+	// double the value of price and return the new price
+	
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
